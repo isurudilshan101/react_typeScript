bb77421a10aac9d5e032bd6912837adf8809ca8b
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2021-06-22 18:41:02 +0530

    typeScript

--- my-app/src/Pages/Page1/Page1Main.tsx
+++ my-app/src/Pages/Page1/Page1Main.tsx
@@ -1,9 +1,11 @@
 import React,{FC} from 'react';
+import { Button } from 'antd';
 
 const Page1Main=()=>{
     return(
         <div>
           aa
+          <Button type="primary" className="button1">Primary Button</Button>
         </div>
     )
 }
