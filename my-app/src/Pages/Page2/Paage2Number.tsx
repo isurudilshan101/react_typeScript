b31f3a6dfa648aa415bf9e3e6d94b01e6c0bca6b
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2021-06-24 04:00:18 +0530

    ui 1

--- my-app/src/Pages/Page2/Paage2Number.tsx
+++ my-app/src/Pages/Page2/Paage2Number.tsx
@@ -5,8 +5,8 @@ import { SearchOutlined } from "@ant-design/icons";
 const Paage2Number = () => {
   return (
     <div>
-      <Button type="primary" shape="circle">
-        1
+      <Button style={{width:"24px",height:"24px",}} type="primary" shape="circle">
+       <span style={{textAlign:"center",}}>1</span> 
       </Button>
     </div>
   );
--- my-app/src/Pages/Page2/Page2Left.tsx
+++ my-app/src/Pages/Page2/Page2Left.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import Paage2Number from './Paage2Number';
 
  const Page2Left=()=> {
   return (
-    <div  style={{width:"131px",height:"70px",}}>
+      <div  style={{ display:"flex",}} >
+          <div  style={{ marginLeft:"0px",}}>
+    <Paage2Number/>
+          </div>
+
+      <div  style={{width:"131px",height:"70px",}}>
       <p  style={{fontWeight:600,fontSize: "16px",}}>Feature Benifit <br/>
       Item Type Details</p>
+      </div>
 
 
+   
     </div>
   );
 }
