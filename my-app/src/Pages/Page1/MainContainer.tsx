559a077a47ae255b8729112dfdc0bd056310d8e2
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2021-06-23 14:22:20 +0530

    table

--- my-app/src/Pages/Page1/MainContainer.tsx
+++ my-app/src/Pages/Page1/MainContainer.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Header from './Header';
+import Page1Table from './Page1Table'
 
  const MainContainer=()=> {
   return (
     <div className="main-container">
       <Header/>
+    <Page1Table/>
+
     </div>
   );
 }
---
+++ my-app/src/Pages/Page1/Page1Table.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Table } from 'antd';
+ 
+  
+ 
+
+const Page1Table=()=> {
+ 
+    return (
+        <div>
+           <Table>
+               
+           </Table>
+        </div>
+    );
+
+}
+export default Page1Table;
