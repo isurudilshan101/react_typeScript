a2b8c31741f0c6b8662d8bdd70658c512e57a115
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2021-06-23 18:14:57 +0530

    test

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,7 +2,6 @@ import React,{FC} from 'react';
  import './App.css';
  import Header from './Pages/Page1/Header';
  import MainContainer from './Pages/Page1/MainContainer';
- 
  import 'antd/dist/antd.css';
  
 
--- my-app/src/Pages/Page1/Page1Table.tsx
+++ my-app/src/Pages/Page1/Page1Table.tsx
@@ -5,12 +5,52 @@ import { Table } from 'antd';
  
 
 const Page1Table=()=> {
+    const columns = [
+        {
+          title: 'Benifit/Feature Type Code',
+          dataIndex: 'name',
+          key:'key',
+       
+        },
+        {
+          title: 'Age',
+          dataIndex: 'age',
+          key:'key',
+             
+          sorter:(a,b)=>a.age-b.age  
+        },
+
+        {
+          title: 'Address',
+          dataIndex: 'address',
+          key:'key'
+        },
+      ];
+
+      const data=[
+          {
+            name:'Isuru',
+            age:'10',
+            address:'Address 1',
+            key:'1'
+          },
+          {
+            name:'Dilshan',
+            age:'20',
+            address:'Address 2',
+            key:'2'
+          },
+      ];
+
+
  
     return (
         <div>
-           <Table>
-               dataSource={[]}
-               columns={[]}
+           <Table className="p1t" dataSource={data}
+            columns={columns}
+           >
+              
+              
               
            </Table>
         </div>
