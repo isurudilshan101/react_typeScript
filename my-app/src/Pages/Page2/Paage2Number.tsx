465af460f639a2e210adbdbb759e03601d51ba5c
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2021-06-24 03:37:19 +0530

    ui 1

---
+++ my-app/src/Pages/Page2/Paage2Number.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Button, Tooltip } from "antd";
+import { SearchOutlined } from "@ant-design/icons";
+
+const Paage2Number = () => {
+  return (
+    <div>
+      <Button type="primary" shape="circle">
+        1
+      </Button>
+    </div>
+  );
+};
+
+export default Paage2Number;
